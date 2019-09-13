7617d880a2b744988bbaac13178a55fd4d152735
Nakkat <benaka4@gmail.com>
2019-09-13 15:09:30 -0300

    Atividade 6 da Aula1 de C#

---
+++ aula1exercicios/Atividade 6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Aula1 {
+    class Program {
+        static void Main (string[] args) {
+
+            int num1, num2;
+            string Fulano;
+            float resultado;
+
+            Console.WriteLine ("Atividade1");
+            Console.Write ("Digite seu nome: ");
+            Fulano = Console.ReadLine ();
+
+            Console.Write ("Digite seu salário: ");
+            num1 = int.Parse (Console.ReadLine ());
+
+            Console.Write ("Digite o valor da sua conta: ");
+            num2 = int.Parse (Console.ReadLine ());
+
+            // Concatenação
+
+            resultado = num1 - num2;
+
+            Console.WriteLine ("O valor restante é: " + resultado);
+
+        }
+    }
+}
