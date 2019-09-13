0a816c791b5e2f929168cfd31c6dddc4143f6ca4
Nakkat <benaka4@gmail.com>
2019-09-13 09:35:36 -0300

    Arquivos Incluidos

---
+++ Aula1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Somente declaramos, sem atribuir valor
+            int num1;
+
+            //Declaramos atribuindo um valor
+            int num2 = 5;
+
+            //Declaramos diversas variáveis de mesmo tipo
+            int num3, num4, num5 = 7;
+
+            // Variável do tipo decimal (real)
+            float media;
+
+            //Variável do tipo lógico (booleana)
+            bool resultado = false;
+
+            Console.WriteLine(num2 + num5);
+        }
+    }
+}
