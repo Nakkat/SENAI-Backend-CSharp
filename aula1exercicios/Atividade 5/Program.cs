fa2c9eecc77ce9aba2f96fbf0b2c6072d70d4594
Nakkat <benaka4@gmail.com>
2019-09-13 15:11:41 -0300

    Atividade 5 da Aula1 de C#

---
+++ aula1exercicios/Atividade 5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula1 {
+    class Program {
+        static void Main (string[] args) {
+
+            int num1, num2, num3;
+            string Fulano;
+            float resultado;
+
+            Console.WriteLine ("Atividade1");
+            Console.Write ("Digite seu nome: ");
+
+            Fulano = Console.ReadLine ();
+
+            Console.WriteLine (Fulano + ", Agora digite o valor do seu salário fixo: ");
+            num1 = int.Parse (Console.ReadLine ());
+
+            Console.WriteLine (Fulano + ", Agora digite o valor das suas vendas: ");
+            num2 = int.Parse (Console.ReadLine ());
+
+            Console.WriteLine (Fulano + ", Agora digite o valor da quantidade de vendas esse mês: ");
+            num3 = int.Parse (Console.ReadLine ());
+
+            resultado = num1 + (num2 * num3) * 10 / 100;
+
+            Console.WriteLine ("Seu salário atual é: " + resultado);
+
+        }
+    }
+}
