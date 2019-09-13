b55cd8e9cfc3f3150122d7dd0bce561a09808b2a
Nakkat <benaka4@gmail.com>
2019-09-13 15:15:05 -0300

    Atividade 2 da aula 1 de C#

---
+++ aula1exercicios/Atividade 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula1 {
+    class Program {
+        static void Main (string[] args) {
+
+            int num1;
+            string Fulano;
+            float resultado;
+
+            Console.WriteLine ("Atividade1");
+            Console.Write ("Digite seu nome: ");
+
+            Fulano = Console.ReadLine ();
+
+            // Concatenação
+            Console.WriteLine (Fulano + ", Agora digite o valor que quer consultar: ");
+
+            // Converto o texto capturado para o tipo inteiro
+            num1 = int.Parse (Console.ReadLine ());
+
+            resultado = num1 * 30 / 100 + num1;
+
+            Console.WriteLine ("Seu valor é: " + resultado);
+
+        }
+    }
+}
