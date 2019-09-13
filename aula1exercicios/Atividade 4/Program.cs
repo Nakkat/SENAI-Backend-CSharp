1a0067f9e08f8bed1d02326c79c912fcd2e2bc9c
Nakkat <benaka4@gmail.com>
2019-09-13 15:12:20 -0300

    Atividade 4 da Aula 1 de C#

---
+++ aula1exercicios/Atividade 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula1 {
+    class Program {
+        static void Main (string[] args) {
+
+            int num1;
+            string Fulano;
+            double resultado;
+
+            Console.WriteLine ("Atividade1");
+            Console.Write ("Digite seu nome: ");
+
+            Fulano = Console.ReadLine ();
+
+            // Concatenação
+            Console.WriteLine (Fulano + ", Agora digite o  valor que você quer consultar: ");
+
+            // Converto o texto capturado para o tipo inteiro
+            num1 = int.Parse (Console.ReadLine ());
+
+            resultado = num1 / 4.07;
+
+            Console.WriteLine ("Seu resultado é: " + resultado);
+
+        }
+    }
+}
