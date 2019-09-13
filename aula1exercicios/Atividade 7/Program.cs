8fd09b277d6ba47692682824a4e0e3e3fea31987
Nakkat <benaka4@gmail.com>
2019-09-13 15:00:52 -0300

    Atividade 7 da Aula1 de C#

---
+++ aula1exercicios/Atividade 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula1 {
+    class Program {
+        static void Main (string[] args) {
+
+        
+            float resultado;
+            int qtd;
+            
+
+            Console.WriteLine ("Atividade7");
+            Console.WriteLine ("Digite quantas pessoas entraram no ônibus");
+            qtd = int.Parse (Console.ReadLine());
+
+
+            resultado = 4.30f * qtd;
+
+            Console.WriteLine ("O valor que você irá receber é: " + resultado);
+
+        }
+    }
+}
