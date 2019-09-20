f706942ad0429b4ad1ac6b4ad363b81312c7caab
Nakkat <benaka4@gmail.com>
2019-09-20 14:02:04 -0300

    Aula4 de c#, parte2

---
+++ aula4-2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace aula4_2 {
+    class Program {
+        static void Main (string[] args) {
+            string[] series1 = new string[10];
+            string[] series2 = new string[10];
+
+            for (int i = 0; i <= 9; i++) {
+
+                Console.WriteLine ("Digite o de uma série");
+                series1[i] = Console.ReadLine ();
+            }
+            for (int i = 0; i <= 9; i++) {
+
+                series2[i] = series1[i];
+            }
+            for (int i = 0; i <= 9; i++) {
+                Console.WriteLine (series2[i]);
+            }
+        }
+    }
+}
