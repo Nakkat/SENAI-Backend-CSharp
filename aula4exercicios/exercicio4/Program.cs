ce200f8c93af9afaf8663bafea13520b8e82c20c
Nakkat <benaka4@gmail.com>
2019-09-23 11:31:57 -0300

    exercicio4 da aula4, adcionado vetor

---
+++ aula4exercicios/exercicio4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vetor1 = new string[15];
+            string[] vetor2 = new string[15];
+            string[] vetor3 = new string[30];
+
+            for (int i = 0; i <= 14; i++) {
+
+                Console.WriteLine ("Digite uma palavra: ");
+                vetor1[i] = (Console.ReadLine ());
+            }
+
+            for (int i = 0; i <= 14; i++) {
+                Console.WriteLine ("Digite uma palavra: ");
+                vetor2[i] = (Console.ReadLine ());
+
+            }
+
+            for (int i = 0; i <= 14; i++) {
+                vetor3[i] = vetor1[i];
+
+            }
+                for (int i = 0; i <= 14; i++) {
+                    vetor3[i+15] = vetor2[i];
+                }
+                 for (int i = 0; i <= 29; i++) {
+                Console.WriteLine (vetor3[i]);
+                 }
+        }
+    }
+}
+        
+    
+
