c67a2c492020cdd7d2a3a68459ef957079f21715
Nakkat <benaka4@gmail.com>
2019-09-23 11:31:14 -0300

    exercicio5 da aula4, adcionado vetor

---
+++ aula4exercicios/exercicio5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor1 = new int [10];
+
+            for(int i = 0; i <= 9; i++) {
+
+                vetor1[i] = (i*2);
+                Console.WriteLine(vetor1[i]);
+            }
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
