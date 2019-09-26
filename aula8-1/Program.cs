fb04af71de6927afca1919d6aeacb8912f328510
Nakkat <benaka4@gmail.com>
2019-09-26 10:51:49 -0300

    Adcionado try, catch

---
+++ aula8-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace aula8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            try{
+
+                // Escrevemos um bloco de código
+            Console.WriteLine("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+            }catch(Exception ex){
+                // Mostramos a excessaõ da regra
+                // Erro gerad na aplicação
+                Console.WriteLine("Ops :( Algo de errado não está certo!" + ex);
+            }
+        }
+    }
+}
