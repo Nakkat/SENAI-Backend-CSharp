12d7c77a0cade02015344bb77a3c0f71aa131cde
Nakkat <benaka4@gmail.com>
2019-09-26 09:40:53 -0300

    Aprendi o que é foreach

---
+++ aula8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace aula8 {
+    class Program {
+        static void Main (string[] args) {
+            string[] v1 = { "A", "B", "C" };
+
+            //Laço FOREACH (para cada)
+            foreach (string letra in v1) {
+
+                Console.WriteLine (letra);
+
+            }
+
+        }
+    }
+
+}
