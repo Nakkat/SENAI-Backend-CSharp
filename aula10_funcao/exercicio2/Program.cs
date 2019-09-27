fe5a4af67253c4f379141a423f84bc3924baf10d
Nakkat <benaka4@gmail.com>
2019-09-27 13:19:56 -0300

    Aula6 Continuação_Exercício2

---
+++ aula10_funcao/exercicio2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImprimeDataHora ();
+            }
+            static void ImprimeDataHora () {
+                Console.WriteLine ("O atual dia da semana é: " + DateTime.Now.ToShortDateString ());
+        }
+    }
+}
