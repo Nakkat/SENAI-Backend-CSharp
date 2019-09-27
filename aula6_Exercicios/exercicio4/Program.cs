8666296ae983443a87973dee7d0f55bdbfa44391
Nakkat <benaka4@gmail.com>
2019-09-27 15:10:27 -0300

    Continuação exercício4 Função

---
+++ aula6_Exercicios/exercicio4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercicio4 {
+    class Program {
+        static void Main (string[] args) {
+
+
+            double[] v1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            System.Console.WriteLine("O maior valor é: " + VetorMaior(v1));
+
+        }
+
+        static double VetorMaior (double[] v1) {
+            double resultado = 0;
+            for (int i = 0; i < v1.Length; i++) {
+                if (v1[i] > resultado) {
+                    resultado = v1[i];
+                }
+            }
+            return resultado;
+        }
+    }
+}
