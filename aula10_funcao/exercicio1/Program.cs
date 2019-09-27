018fedc720d25278d8ff6b1cf865a59c77a05f61
Nakkat <benaka4@gmail.com>
2019-09-27 13:18:41 -0300

    aula6 - Continuação_Exercício1

---
+++ aula10_funcao/exercicio1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace aula10_funcao {
+    class Program {
+        //Criar uma função do tipo double que
+        //recebe como argumentos valor e
+        //porcentagem, onde o intuito é atribuir um desconto sobre a porcentagem e retornar
+        //o valor aplicado o desconto
+        static void Main (string[] args) {
+            double valor = 0;
+            double porcentagem = 0;
+
+            Console.WriteLine ("Digite o valor: ");
+            valor = double.Parse (Console.ReadLine ());
+
+            Console.WriteLine ("Digite uma porcentagem: ");
+            porcentagem = double.Parse (Console.ReadLine ());
+
+            Console.WriteLine("Seu valor final é: " + aplicaDesconto(porcentagem, valor));
+
+        }
+
+        static double aplicaDesconto (double porcentagem, double valor) {
+            double desconto = 0;
+            porcentagem = porcentagem / 100;
+            desconto = valor * porcentagem;
+            valor = valor - desconto;   
+            return valor;
+        }
+
+    }
+}
