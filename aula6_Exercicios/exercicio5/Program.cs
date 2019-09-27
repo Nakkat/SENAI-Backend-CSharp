424d8b6c33ef15011d83717e76a52c6257631bbf
Nakkat <benaka4@gmail.com>
2019-09-27 15:11:09 -0300

    Coninuação exercício5 Função

---
+++ aula6_Exercicios/exercicio5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] v1 = {1, 2, 3, 4, 5, 6};
+            Console.WriteLine("O valor total é: " + ValorTotal(v1));
+        }
+
+        static double ValorTotal (double[] v1) {
+            double resultado = 0;
+            for (int i = 0; i < v1.Length; i++) {
+                resultado = resultado + v1[i];
+            }
+                return resultado;
+            }
+        }
+    }
+
